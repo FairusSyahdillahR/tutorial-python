187ac758570fd777e4d8eac5c5c38cdf511ee06b
FairusSyahdillahR <140950652+FairusSyahdillahR@users.noreply.github.com>
2024-02-25 18:40:13 +0700

    Menghitung di python

---
+++ hitung.py
@@ -0,0 +1,23 @@
+a = 10
+b = 3
+
+hasil = a+b
+print('Hasil dari penjumlahan tersebut adalah :',hasil)
+
+hasil = a-b
+print('Hasil dari pengurangan tersebut adalah :',hasil)
+
+hasil = a*b
+print('Hasil dari perkalian tersebut adalah :',hasil)
+
+hasil = a/b
+print('Hasil dari pembagian tersebut adalah :',hasil)
+
+hasil = a**b
+print('Hasil dari eksponen (pangkat) tersebut adalah :',hasil)
+
+hasil = a%b
+print('Hasil dari modulus tersebut adalah :',hasil)
+
+hasil = a//b
+print('Hasil dari floor division tersebut adalah :',hasil)
