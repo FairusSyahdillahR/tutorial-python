3ab702302b03d2bc5f357b3a5df019262ff321ec
FairusSyahdillahR <140950652+FairusSyahdillahR@users.noreply.github.com>
2024-02-25 18:40:53 +0700

    Tipe data pada python

---
+++ tipe_data.py
@@ -0,0 +1,29 @@
+a=10
+x = 2
+b = 1.5
+c = "asep"
+panjang = 100
+
+# integer (angka)
+print(type(a))
+print("Nilai A =", a, "bertipe :", type(a))
+print("Nilai X =", x)
+print("Nilai panjang =", panjang)
+
+#float (angka dengan tanda koma)
+print(type(b))
+print("Nilai B =", b, "bertipe :", type(b))
+
+#string (karakter)
+print(type(c))
+print("Nilai C =", c, "bertipe :", type(c))
+
+#boolean (true/false)
+d = True
+print(type(d))
+print("Nilai C =", d, "bertipe :", type(d))
+
+## tipe data khusus
+e = complex (5,6)
+print(e)
+print("Nilai e =", e, "bertipe :", type(e))
