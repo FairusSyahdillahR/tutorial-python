d8f13c29c5d95c6a351911c2725776d5b1ef6cab
FairusSyahdillahR <140950652+FairusSyahdillahR@users.noreply.github.com>
2024-02-25 18:35:29 +0700

    Casting data python
    Tutorial untuk casting data di python

---
+++ casting_data.py
@@ -0,0 +1,14 @@
+data_int = 9;
+print("data =", data_int, ",type =", type(data_int))
+
+data_float = float(data_int)
+print("data =", data_float, ",type =", type(data_float))
+
+data_str = str(data_int) 
+print("data =", data_str, ",type =", type(data_str))
+
+# akan false apabila angka nya 0
+data_bool = bool(data_int)
+print("data =", data_bool, ",type =", type(data_bool))
+
+
