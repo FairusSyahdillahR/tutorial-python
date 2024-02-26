7e77e965b852438acb391c332bbaceef2fbf7985
FairusSyahdillahR <140950652+FairusSyahdillahR@users.noreply.github.com>
2024-02-26 18:01:46 +0700

    Program python untuk konversi suhu Celcius

---
+++ Porgram konversi suhu Celcius.py
@@ -0,0 +1,16 @@
+print('##---- PROGRAM MENGHITUNG SUHU ----##')
+print('=====================================')
+print('=====================================')
+print()
+print()
+
+c = float(input('Masukkan Input suhu Celcius :'))
+
+
+f=((9/5)*c)+32
+k=c +273
+r=(4/5) * c
+
+print(c,'derajat C =',f,'derajat Fahrenheit')
+print(c,'derajat C =',k,'derajat Kelvin')
+print(c,'derajat C =',r,'derajat Reamur')
