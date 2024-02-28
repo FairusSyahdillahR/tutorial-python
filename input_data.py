106ce04f1e867aaf6ae6d3065da7c390a2f1b436
FairusSyahdillahR <140950652+FairusSyahdillahR@users.noreply.github.com>
2024-02-28 12:50:23 +0700

    Cara Input data di python

---
+++ input_data.py
@@ -0,0 +1,11 @@
+#data yang dimasukkan pasti string
+data = input("Masukkan data :")
+print("data =", data, "type =",type(data))
+
+#jika ingin integer
+data_int = int (input("Masukkan data :"))
+print("data =", data_int, "type =",type(data_int))
+
+#boolean
+data_bool = bool(int(input("Masukkan Data:")))
+print("data =", data_bool, "type =",type(data_bool))
